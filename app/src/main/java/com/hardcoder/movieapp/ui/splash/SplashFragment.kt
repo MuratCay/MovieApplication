9e92a9d8712b699d87dd4bcf729f633f74ace621
ebelli <ebelli@invio.com>
2022-07-02 19:32:52 +0300

    Splash screen done.

--- app/src/main/java/com/hardcoder/movieapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/hardcoder/movieapp/ui/splash/SplashFragment.kt
@@ -1,21 +1,39 @@
 package com.hardcoder.movieapp.ui.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.hardcoder.movieapp.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.hardcoder.movieapp.core.BaseFragment
 import com.hardcoder.movieapp.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
 
+    private val viewModel by viewModels<SplashViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initObservers()
 
+    }
+
+    private fun navigateToFlow() {
+        val action = SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun initObservers() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.viewEffect.collect {
+                when (it) {
+                    is SplashViewEffect.PassToLoginPage -> navigateToFlow()
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/hardcoder/movieapp/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/hardcoder/movieapp/ui/splash/SplashViewModel.kt
@@ -1,9 +1,32 @@
 package com.hardcoder.movieapp.ui.splash
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor() : ViewModel() {
+
+    private var _viewEffect = MutableSharedFlow<SplashViewEffect>()
+    val viewEffect = _viewEffect.asSharedFlow()
+
+    init {
+        createSplashDuration()
+    }
+
+    private fun createSplashDuration() {
+        viewModelScope.launch {
+            delay(5000)
+            _viewEffect.emit(SplashViewEffect.PassToLoginPage)
+        }
+    }
+}
+
+sealed class SplashViewEffect {
+    object PassToLoginPage : SplashViewEffect()
 }
