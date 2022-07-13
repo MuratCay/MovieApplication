cc7ede346324043e7606ba0ef4abd31c2adfcd8d
muratcay <muratthebestof@gmail.com>
2022-07-13 16:36:10 +0300

    created bottom navigation and only appear on used pages

--- app/src/main/java/com/hardcoder/movieapp/MainActivity.kt
+++ app/src/main/java/com/hardcoder/movieapp/MainActivity.kt
@@ -2,12 +2,82 @@ package com.hardcoder.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hardcoder.movieapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.favoriteFragment,
+                R.id.profileFragment,
+            )
+        )
+
+        setupWithNavController(binding.bottomNavigationView, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (appBarConfiguration.topLevelDestinations.contains(destination.id)) {
+                binding.bottomNavigationView.isVisible = true
+            } else {
+                binding.bottomNavigationView.isGone = true
+            }
+        }
+    }
+
+    fun hideBottomNavigation(recyclerView: RecyclerView) {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0 && binding.bottomNavigationView.isShown) {
+                    slideDown(binding.bottomNavigationView)
+                } else if (dy < 0) {
+                    slideUp(binding.bottomNavigationView)
+                }
+            }
+        })
+    }
+
+    private fun slideUp(child: BottomNavigationView) {
+        child.clearAnimation()
+        child.animate().translationY(0f).duration = 150
+    }
+
+    private fun slideDown(child: BottomNavigationView) {
+        child.clearAnimation()
+        child.animate().translationY(binding.bottomNavigationView.height.toFloat()).duration = 220
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
