7b42953e42672bb919d2ff7d44351742d81ebe73
ebelli <ebelli@invio.com>
2022-07-24 02:15:09 +0300

    movie detail and fonts are created.

---
+++ app/src/main/java/com/hardcoder/movieapp/ui/home/MovieDetailFragment.kt
@@ -0,0 +1,10 @@
+package com.hardcoder.movieapp.ui.home
+
+import com.hardcoder.movieapp.R
+import com.hardcoder.movieapp.databinding.FragmentMovieDetailBinding
+import com.hardcoder.movieapp.ui.base.BaseFragment
+
+class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding>() {
+
+    override fun getFragmentView(): Int = R.layout.fragment_movie_detail
+}
